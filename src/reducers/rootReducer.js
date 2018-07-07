e991d185576ac7305c5360a64bfb792198491742
Mauro Lavigna <mauro.lavigna@gmail.com>
2018-07-07 14:35:13 -0300

    adding ajax status actions, reducer, and a common component while loading

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import authors from './authorReducer';
 import courses from './courseReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   authors,
-  courses
+  courses,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
