066604156afe095e5c22c776c236d1a11f7e0e11
Mauro Lavigna <mauro.lavigna@gmail.com>
2018-07-07 02:52:59 -0300

    separating courses into different presentational and container components. adding redux-thunk to display data from the mock API

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
+import {loadCourses} from './actions/courseActions';
 import 'babel-polyfill';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Can pass InitialState from the server to override custom initial state on reducers.
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import reduxInmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxInmutableStateInvariant())
+    applyMiddleware(thunk, reduxInmutableStateInvariant())
   );
 }
