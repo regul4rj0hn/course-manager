61f4bbbff07931a3f7ce8a7af5d0ee387d2c853f
Mauro Lavigna <mauro.lavigna@gmail.com>
2018-07-07 01:34:38 -0300

    adding boilerplate code for redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import 'babel-polyfill';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
+import 'babel-polyfill';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+// Can pass InitialState from the server to override custom initial state on reducers.
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
