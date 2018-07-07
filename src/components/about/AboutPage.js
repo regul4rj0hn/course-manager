e69e41bb95c6815e72f6e82c6739e0f5eed3eac7
Mauro Lavigna <mauro.lavigna@gmail.com>
2018-07-07 00:58:02 -0300

    initial app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Course manager React application for educational purposes.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
